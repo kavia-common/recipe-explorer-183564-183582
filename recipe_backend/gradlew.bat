c6bc661c89ac96ba1e4074131e06d809c51f32ed
Kavia AI <production@kavia.ai>
2025-11-05 13:50:38 +0000

    Checkpoint 4 - cm67c018e3

--- recipe_backend/gradlew.bat
+++ recipe_backend/gradlew.bat
@@ -1,67 +1,34 @@
 @ECHO OFF
 @rem ##########################################################################
-@rem
-@rem  Gradle startup script for Windows
-@rem
+@rem Gradle startup script for Windows (text-mode wrapper for CI)
 @rem ##########################################################################
+setlocal
 
-@rem Set default JVM options
+@rem Default JVM options
 set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 
 set DIRNAME=%~dp0
-if "%DIRNAME%" == "" set DIRNAME=.
-set APP_BASE_NAME=%~n0
-set APP_HOME=%DIRNAME%
+if "%DIRNAME%"=="" set DIRNAME=.
+set "APP_HOME=%DIRNAME%"
+set "APP_BASE_NAME=%~n0"
 
-set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+set "CLASSPATH=%APP_HOME%gradle\wrapper\gradle-wrapper.jar"
 
-@rem Find java.exe
-if defined JAVA_HOME goto findJavaFromJavaHome
+@rem Find Java
+if defined JAVA_HOME (
+  set "JAVA_EXE=%JAVA_HOME%\bin\java.exe"
+) else (
+  set "JAVA_EXE=java.exe"
+)
 
-set JAVA_EXE=java.exe
-%JAVA_EXE% -version >NUL 2>&1
-if "%ERRORLEVEL%" == "0" goto init
+"%JAVA_EXE%" -version >NUL 2>&1
+if not "%ERRORLEVEL%"=="0" (
+  echo ERROR: JAVA_HOME is not set and no 'java' command could be found on PATH.
+  exit /b 1
+)
 
-echo.
-echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
-echo.
-echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java installation.
-goto fail
+@rem Execute Gradle Wrapper Main
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% -Dorg.gradle.appname=%APP_BASE_NAME% -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+set EXIT_CODE=%ERRORLEVEL%
 
-:findJavaFromJavaHome
-set JAVA_HOME=%JAVA_HOME:"=%
-set JAVA_EXE=%JAVA_HOME%\bin\java.exe
-
-if exist "%JAVA_EXE%" goto init
-
-echo.
-echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
-echo.
-echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java installation.
-goto fail
-
-:init
-@rem Get command-line arguments, handling Windows variants
-
-set CMD_LINE_ARGS=
-set _SKIP=2
-
-:win8_fix
-if "x%1" == "x" goto execute
-
-set CMD_LINE_ARGS=%*
-goto execute
-
-:execute
-@rem Setup the command line
-
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% -Dorg.gradle.appname=%APP_BASE_NAME% -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
-goto end
-
-:fail
-exit /b 1
-
-:end
-exit /b 0
+endlocal & exit /b %EXIT_CODE%
